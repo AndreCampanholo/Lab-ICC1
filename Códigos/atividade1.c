6b528f08a02f597a2ccc72403dbbfa26f9bc3de2
André Campanholo Paschoalini <paschoaliniandre@gmail.com>
2025-07-15 14:08:36 -0300

    Update atividade1.c

--- Códigos/atividade1.c
+++ Códigos/atividade1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-  int n;
-  scanf("%d", &n);
+  int numero; //cria a variável inteira 'numero'
+  scanf("%d", &numero); //lê na entrada um número informado pelo usuário e atribui à variável 'numero'
 
-  printf("Hello, World! %d\n", n);
+  printf("Hello, World! %d\n", numero); //imprime o número informado ao final do texto
 
-  return 0;
+  return 0; //indica sucesso e finaliza a execução do código
 }
