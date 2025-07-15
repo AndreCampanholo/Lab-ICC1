3eccc4cc4e01fd249ca7eb53d3161720f7081b75
André Campanholo Paschoalini <paschoaliniandre@gmail.com>
2025-07-15 14:06:40 -0300

    Update atividade2.c

--- Códigos/atividade2.c
+++ Códigos/atividade2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    char nome[50];
-    scanf("%49[^\n]", nome);
+    char nome[50]; //cria uma string 'nome': vetor de 50 caracteres, 49 deles válidos e o terminador nulo \0 (indica o fim do vetor) para receber o nome da pessoa
+    scanf("%49[^\n]", nome); //lê na entrada um nome de no máximo 49 caracteres e o atribui à string 'nome'
 
-    printf("Bom dia, %s!\n", nome);
+    printf("Bom dia, %s!\n", nome); //imprime o nome informado ao final do texto
 
-    return 0;
+    return 0; //indica sucesso e finaliza a execução do código
 }
