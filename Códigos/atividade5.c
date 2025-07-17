ec7a37719c3f0ff51b774eba1fa6e9dd2135cec8
André Campanholo Paschoalini <paschoaliniandre@gmail.com>
2025-07-17 18:15:21 -0300

    Update atividade5.c

--- Códigos/atividade5.c
+++ Códigos/atividade5.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c, maior;
+    int a, b, c, maior; //cria variáveis inteiras para 3 números de entrada e para o maior entre eles
     scanf("%d", &a);
-    scanf("%d", &b);
+    scanf("%d", &b); //leitura das 3 variáveis de entrada
     scanf("%d", &c);
 
     if(a >= b && a >= c) {
         maior = a;
     } else if(b >= a && b >= c) {
-        maior = b;
+        maior = b;                 //laço que determina qual dos 3 números é o maior entre eles e atribui esse valor à variável 'maior'
     } else  if(c >= a && c >= b) {
         maior = c;
     }
 
-    printf("%d\n", maior);
+    printf("%d\n", maior); //imprime o maior entre os 3 números inteiros
 
     return 0;
 }
